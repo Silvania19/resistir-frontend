ffa6312d5ac4f765c08f5da6891b99584078faf1
Silvania19 <silvaniaortega17@gmail.com>
2023-06-04 23:18:02 -0300

    add home and see temperature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TemperatureAddComponent } from './components/temperature-add/temperature-add.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'addTemperature', component: TemperatureAddComponent},
+  { path : 'home', component: HomeComponent},
+  {path: "**", redirectTo: "", component: HomeComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/temperature-add/temperature-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-temperature-add',
+  templateUrl: './temperature-add.component.html',
+  styleUrls: ['./temperature-add.component.css']
+})
+export class TemperatureAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
