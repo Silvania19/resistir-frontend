973a701f746b0778614c4f9daa05f560bb33a425
Ignacio Chiaradia <ignachy01@gmail.com>
2024-04-01 13:34:26 -0300

    Add navbar and post of temperatures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { TemperatureAddComponent } from './components/temperature-add/temperature-add.component';
 import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule} from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TemperatureAddComponent,
-    HomeComponent  ],
+    HomeComponent,
+    NavbarComponent  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/temperature-add/temperature-add.component.ts
+++ src/app/components/temperature-add/temperature-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { TemperatureServiceService } from 'src/app/services/temperature-service.service';
 
 @Component({
   selector: 'app-temperature-add',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemperatureAddComponent implements OnInit {
 
-  constructor() { }
+  formTemp: FormGroup = new FormGroup({})
+
+  constructor(private tempService: TemperatureServiceService) { }
 
   ngOnInit(): void {
+    this.formTemp = new FormGroup({
+      minTemp: new FormControl(null,[Validators.required]),
+      maxTemp: new FormControl(null,[Validators.required])
+    })
+  }
+
+  saveRange(){
+    this.tempService.setTemperature(this.formTemp.value);
   }
 
 }
