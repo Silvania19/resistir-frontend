40e0f1caf3e148ef8e40a0d3a6e2cf89c69845bc
Ignacio Chiaradia <ignachy01@gmail.com>
2024-05-10 00:10:02 -0300

    Modified form of temps
    Some validatios were added to the form of range of temperatures and some messages in case of bad ussing of the form.

--- src/app/components/temperature-add/temperature-add.component.ts
+++ src/app/components/temperature-add/temperature-add.component.ts
@@ -12,21 +12,36 @@ export class TemperatureAddComponent implements OnInit {
 
   formTemp: FormGroup = new FormGroup({})
 
+  public tempMessage: string = "";
+
   constructor(private tempService: TemperatureServiceService) { }
 
   ngOnInit(): void {
     this.formTemp = new FormGroup({
-      minTemp: new FormControl(null,[Validators.required]),
-      maxTemp: new FormControl(null,[Validators.required])
+      minTemp: new FormControl(null,[Validators.required, Validators.min(15), Validators.max(25)]),
+      maxTemp: new FormControl(null,[Validators.required, Validators.min(20), Validators.max(30)])
     })
+
+    this.formTemp.valueChanges.subscribe((value : any) => {
+      
+      if(value.minTemp != null && value.maxTemp != null){
+        if(value.maxTemp < value.minTemp){
+          this.tempMessage = "Maximo no puede ser menor a minimo"          
+        } else if(this.formTemp.invalid){
+          this.tempMessage = "error de rango";          
+        }
+        else{
+          this.tempMessage = "";
+        }
+      }
+    })    
   }
 
   saveRange(){
     let setRangeTemperature = new SaveTemperature();
     setRangeTemperature.maxTemperature = this.formTemp.value.maxTemp;
     setRangeTemperature.minTemperature = this.formTemp.value.minTemp;
-   this.tempService.setRangeTemperature(setRangeTemperature).subscribe();
-    
+    this.tempService.setRangeTemperature(setRangeTemperature).subscribe();
   }
 
 }
